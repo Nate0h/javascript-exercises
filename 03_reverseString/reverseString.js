ae5a7fc405176ff3f6faff6b5ceb0ef9a38a270d
Nathaniel Colon <nathanielcolon12@gmail.com>
2023-04-17 23:25:52 -0400

    reverse string code exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let my_array = string.split("");
+my_array.reverse();
+let output = my_array.join("");
+return output;
 };
 
 // Do not edit below this line
