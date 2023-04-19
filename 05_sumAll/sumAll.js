a9ec8f5cc8cc06720866591fd7f5dcee08fb3069
Nathaniel Colon <nathanielcolon12@gmail.com>
2023-04-18 20:41:49 -0400

    sumAll code exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+let sum = 0;
+let lo,hi;
+if(num1 < 0 || num2 < 0){
+    return "ERROR";
+}
 
+if(typeof num1 != "number" || typeof num2 != "number" ){
+    return "ERROR"
+}
+if(num1 < num2){
+     lo = num1;
+     hi = num2;
+}
+else if(num2 < num1){
+     lo = num2;
+     hi = num1;
+}
+
+while(lo <= hi){
+    sum += lo;
+    lo++;
+}
+return sum;
 };
 
 // Do not edit below this line
